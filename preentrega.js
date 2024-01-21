b7c7b186f2f41e375b82118936174d54c2845ded
Evelyn Ferreyra <ferreyraevelyn.26@gmail.com>
2024-01-21 18:50:56 -0300

    primer preentrega

---
+++ preentrega.js
@@ -0,0 +1,11 @@
+//console.log("Hola Eve, estoy aprendiendo a programar")
+//console.log("Hola Estoy aprendiendo a programar")
+//console.log("Hola yo tambien estoy aprendiendo a programar")
+
+function saludarATodos() {
+    let nombre = prompt ("a quien queres saludar")
+    console.log("Hola" + nombre + "estoy aprendiendo a programar")
+
+}
+
+saludarATodos()
